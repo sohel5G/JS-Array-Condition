6471b889e0e76b066fcc97427e0769bfc89988fd
sohel5G <contactsohelrana>
2023-07-30 21:24:39 +0600

    3 task done

--- task.js
+++ task.js
@@ -1,4 +1,29 @@
-var fruits = ['Apple', 'Banana', 'Orange'];
+// Task one 
 
-console.log( fruits[1] );
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// var findBananaIndex = fruits.indexOf('Banana');
+// console.log('Banana index is ' + findBananaIndex);
+
+// Task 2 
+
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// fruits[1] = 'Mango';
+// console.log(fruits);  //Raplace Banana with Mango
+
+// Task 3 
+
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// fruits.pop();
+// console.log(fruits);
+// fruits.push('Watermelon');
+// console.log(fruits);
+
+
+// Task 4 
+
+
+
+
+
+
 
